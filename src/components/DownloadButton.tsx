43b866f3645d95005a120be1ebe94731a3234f18
ktaroabobon <ktaroabobon@gmail.com>
2023-08-11 15:38:46 +0900

    update: Reviewを元に修正

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import CustomIcon from "../icons/CustomIcon";
 import { Button, Text } from "@chakra-ui/react";
-const MyDownloadButton = ({ url }: { url: string }) => {
+
+type MyDownloadButtonProps = {
+  url: string;
+};
+
+const MyDownloadButton: React.FC<MyDownloadButtonProps> = ({ url }) => {
   // ボタンがクリックされたかどうかを追跡するための状態
-  const [isClicked, setIsClicked] = useState(false);
-
-  // ボタンがクリックされたときに呼び出されるハンドラ
-  const handleClick = () => {
-    setIsClicked(true);
-  };
-
   return (
     <Button
       variant="solid"
@@ -17,8 +15,6 @@ const MyDownloadButton = ({ url }: { url: string }) => {
       as="a"
       href={url} // 引数から受け取ったURL
       download
-      onClick={handleClick}
-      isDisabled={isClicked} // 状態に基づいてボタンを無効にする
     >
       <CustomIcon icon={"download"} />
       <Text ml={1}>Download</Text>
